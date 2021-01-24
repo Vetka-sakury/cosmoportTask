ac20e897772328fd98fb6ae30307afdab0553267
Vetka-sakury <77798639+Vetka-sakury@users.noreply.github.com>
2021-01-24 14:39:37 +0300

    Update ShipController.java

--- src/main/java/com/space/controller/ShipController.java
+++ src/main/java/com/space/controller/ShipController.java
@@ -1,8 +1,13 @@
 package com.space.controller;
 
 import com.space.model.Ship;
+import com.space.model.ShipType;
 import com.space.service.ShipService;
+import com.space.service.ShipSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +16,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static com.space.utils.ValidationUtils.validateId;
+import static com.space.utils.ShipUtil.transformDataForUpdate;
+import static com.space.utils.ValidationUtils.*;
 
 @RestController
 @RequestMapping(value = "/rest/ships", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -21,35 +27,84 @@ public class ShipController {
     @Autowired
     ShipService shipService;
 
-    @GetMapping (value = "/{id}")
-    public ResponseEntity <Ship> getShipById (@PathVariable Long shipId){
-        if (!validateId(shipId)){
+    @GetMapping("/count")
+    public ResponseEntity<Long> getCountFiltered(@RequestParam(required = false) String name, @RequestParam(required = false) String planet,
+                                 @RequestParam(required = false) ShipType shipType, @RequestParam(required = false) Boolean isUsed,
+                                 @RequestParam(required = false) Long after, @RequestParam(required = false) Long before,
+                                 @RequestParam(required = false) Double minSpeed, @RequestParam(required = false) Double maxSpeed,
+                                 @RequestParam(required = false) Integer minCrewSize, @RequestParam(required = false) Integer maxCrewSize,
+                                 @RequestParam(required = false) Double minRating, @RequestParam(required = false) Double maxRating) {
+        Long count = shipService.getShipsCount(ShipSpecification.allSpecification(name, planet, shipType, isUsed,
+                after, before, minSpeed, maxSpeed, minCrewSize, maxCrewSize, minRating, maxRating));
+        return new ResponseEntity<>(count, HttpStatus.OK);
+    }
+
+
+    @GetMapping(value = "")
+    public ResponseEntity<List<Ship>> getShipsByFilter (@RequestParam(required = false) String name, @RequestParam(required = false) String planet,
+                                        @RequestParam(required = false) ShipType shipType, @RequestParam(required = false) Boolean isUsed,
+                                        @RequestParam(required = false) Long after, @RequestParam(required = false) Long before,
+                                        @RequestParam(required = false) Double minSpeed, @RequestParam(required = false) Double maxSpeed,
+                                        @RequestParam(required = false) Integer minCrewSize, @RequestParam(required = false) Integer maxCrewSize,
+                                        @RequestParam(required = false) Double minRating, @RequestParam(required = false) Double maxRating,
+                                        @RequestParam(defaultValue = "ID") ShipOrder order,
+                                        @RequestParam(defaultValue = "0") Integer pageNumber, @RequestParam(defaultValue = "3") Integer pageSize) {
+        Sort sort = new Sort(Sort.Direction.ASC, order.getFieldName());
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
+        List<Ship> list = shipService.getByFilter(ShipSpecification.allSpecification(name, planet, shipType, isUsed, after, before, minSpeed, maxSpeed, minCrewSize, maxCrewSize, minRating, maxRating), pageable);
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Ship> createShip (@RequestBody Ship ship){
+        if (checkEmptyBody(ship)
+                || !validateShipDataForCreate(ship))
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(shipService.createShip(ship), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/{id}")
+    public ResponseEntity <Ship> updateShip (@RequestBody Ship ship, @PathVariable Long id){
+        if (!validateId(id) || ship == null){
+            return new ResponseEntity<> (HttpStatus.BAD_REQUEST);
+        }
+        if (!validateShipDataForUpdate(ship)){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        Ship ship = shipService.getById(shipId);
+        Ship shipFound = shipService.getById(id);
+        if (checkEmptyBody(ship)){
+            return new ResponseEntity<>(shipFound, HttpStatus.OK);
+        }
+        if (shipFound == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Ship updated = shipService.update(transformDataForUpdate(shipFound, ship), id);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+
+    @GetMapping (value = "/{id}")
+    public ResponseEntity <Ship> getShipById (@PathVariable Long id){
+        if (!validateId(id)){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Ship ship = shipService.getById(id);
         if (ship == null){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(ship, HttpStatus.OK);
     }
 
-    @GetMapping
-    public ResponseEntity <List <Ship>> getAllShips(){
-        List <Ship> ships= shipService.getAll();
-        return new ResponseEntity<>(ships, HttpStatus.OK);
-    }
-
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity <Ship> deleteShip (@PathVariable Long shipId){
+    public ResponseEntity <Ship> deleteShip (@PathVariable Long id){
 
-        if (!validateId(shipId)){
+        if (!validateId(id)){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        Ship ship = shipService.getById(shipId);
+        Ship ship = shipService.getById(id);
         if (ship == null){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        shipService.delete(shipId);
+        shipService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
