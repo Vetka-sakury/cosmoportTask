eaa9c16d0c9c189c6ca277dbe0d6b4bb970a3bad
Vetka-sakury <77798639+Vetka-sakury@users.noreply.github.com>
2021-01-24 14:40:56 +0300

    Update ShipService.java

--- src/main/java/com/space/service/ShipService.java
+++ src/main/java/com/space/service/ShipService.java
@@ -1,15 +1,17 @@
 package com.space.service;
 
+
 import com.space.model.Ship;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 
 public interface ShipService {
-    List <Ship> getByFilter();
-    List<Ship> getAll();
-    Integer getShipsCount();
-    Ship createShip();
+    List <Ship> getByFilter(Specification<Ship> specification, Pageable page);
+    Long getShipsCount (Specification<Ship> specification);
+    Ship createShip(Ship ship);
     Ship getById(Long id);
-    void updateShip(Long id);
+    Ship update(Ship ship, Long id);
     void delete (Long id);
 }
